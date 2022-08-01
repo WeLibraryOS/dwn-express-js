70bf712e97da90dc08d2308c6aa040ca071ecd5b
Peter O'Leary <pete@timelight.com>
2022-07-31 17:16:25 -0700

    Add note about type: module in package.json

--- source/index.js
+++ source/index.js
@@ -4,12 +4,13 @@ const port = 8080; // default port to listen
 
 // case #1 DWN will be a promise that when resolved throws
 // Uncaught Error Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/pete_o/Documents/Dev/learn_card/dwn-sdk-js/dist/esm/validation/validator' imported from /Users/pete_o/Documents/Dev/learn_card/dwn-sdk-js/dist/esm/dwn.js
-// const DWN   = import('dwn-sdk');
+const DWN   = import('dwn-sdk');
 
 // case #2 DWN resolves but is an empty object 
-const DWN   = require('dwn-sdk');
+// const DWN   = require('dwn-sdk');
 
 // case #3 throws Uncaught Error Error [ERR_MODULE_NOT_FOUND]: Cannot find module '/Users/pete_o/Documents/Dev/learn_card/dwn-sdk-js/dist/esm/validation/validator' imported from /Users/pete_o/Documents/Dev/learn_card/dwn-sdk-js/dist/esm/dwn.js
+// NOTE: you need to set "type": "module" in package.json first
 // import { DWN } from "dwn-sdk";
 
 // define a route handler for the default home page
