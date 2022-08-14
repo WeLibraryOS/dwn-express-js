f2a13498c1be973b7e6584e742bd5f5cc64d464d
Peter O'Leary <pete@mudflapinc.com>
2022-08-13 20:04:24 -0700

    Add some notes to README, figure out how to initialize with Config

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Steps to reproduce
+# Installation instructions
 
 Link you local copy of dwn-sd-js here as described  in https://github.com/TBD54566975/dwn-sdk-js/blob/main/README.md
 
@@ -6,3 +6,50 @@ Link you local copy of dwn-sd-js here as described  in https://github.com/TBD545
 npm install
 npm run start
 ```
+
+## What are we trying to do here?
+
+Store and retrieve Verifiable Credential documents
+
+## Notes on SDK
+
+## Config object
+
+Describe what is the Config object used to initialize the SDK
+
+```
+export type Config = {
+  DIDMethodResolvers: DIDMethodResolver[],
+  interfaces?: Interface[];
+  messageStore?: MessageStore;
+};
+```
+
+### DIDMethodResolvers
+
+A `DIDMethodResolver` consists of:
+```
+method(): string;
+resolve(did: string): Promise<DIDResolutionResult>
+```
+
+A "resolver" takes a DID as input and produces a DID Document
+
+### Interface
+
+``` Javscript
+export interface Interface {
+  methodHandlers: MethodHandler[];
+  schemas: { [key:string]: object };
+}
+
+export type MethodHandler = (
+  ctx: Context,
+  message: BaseMessageSchema,
+  messageStore: MessageStore,
+  didResolver: DIDResolver) => Promise<MessageReply>;
+```
+
+### MessageStore
+
+What is this?
--- source/index.ts
+++ source/index.ts
@@ -1,5 +1,11 @@
 import { DWN } from "dwn-sdk";
+import { MessageReply } from "dwn-sdk/dist/esm/core";
+import { BaseMessageSchema } from "dwn-sdk/dist/esm/core/types";
+import { DIDMethodResolver, DIDResolutionResult, DIDResolver } from "dwn-sdk/dist/esm/did/did-resolver";
+import {Config} from "dwn-sdk/dist/esm/dwn";
 import { Interface } from "dwn-sdk/dist/esm/interfaces/types";
+import { MessageStore } from "dwn-sdk/dist/esm/store/message-store";
+import { Context } from "dwn-sdk/dist/esm/types";
 import express, { Application, Request, Response, NextFunction } from "express";
 
 
@@ -7,12 +13,44 @@ const app: Application = express();
 app.use(express.json());
 const port = 8080; // default port to listen
 
-const schemas = {} as { [key: string]: any };
+class VCMethodResolver implements DIDMethodResolver {
 
+  method(): string {
+    return "vc";
+  }
+
+  async resolve(did: string): Promise<DIDResolutionResult> {
+    return {
+      '@context': 'https://w3id.org/did-resolution/v1',
+      didResolutionMetadata: {
+        contentType: 'string'
+      },
+      didDocument: null,
+      didDocumentMetadata: {} // DIDDocumentMetadata
+    };
+  }
+}
+
+const schemas = {} as { [key: string]: any };
 schemas['FeatureDetectionRead'] = true;
 
-const config = {
-  interfaces:  [{methodHandlers: [], schemas: schemas}]
+function FeatureDetectionRead(
+  ctx: Context,
+  message: BaseMessageSchema,
+  messageStore: MessageStore,
+  didResolver: DIDResolver): Promise<MessageReply> {
+  return Promise.resolve(new MessageReply({status: {code: 200, message: 'OK'}}));
+}
+
+const methodHandlers = [] 
+methodHandlers['FeatureDetectionRead'] = FeatureDetectionRead;
+
+const config: Config = {
+  DIDMethodResolvers: [new VCMethodResolver()],
+  interfaces: [{
+    methodHandlers: methodHandlers,
+    schemas: schemas
+  }]
 }
 
 // TODO: maybe wrapp this in a class
