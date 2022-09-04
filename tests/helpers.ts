5e45a98a6d4b8a38a404d7c498a1356c14112950
Peter O'Leary <pete@mudflapinc.com>
2022-09-03 19:42:05 -0700

    Basica CollectionsWrite test

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -1,3 +1,4 @@
+import { CID } from "multiformats/cid";
 import { DIDMethodResolver, DIDResolutionResult } from "../source/dwn-sdk/did/did-resolver";
 import { secp256k1 } from "../source/dwn-sdk/jose/algorithms/signing/secp256k1";
 import { GeneralJwsSigner, GeneralJwsVerifier } from "../source/dwn-sdk/jose/jws/general";
@@ -59,4 +60,42 @@ export class TestMethodResolver implements DIDMethodResolver {
     async resolve(did: string): Promise<DIDResolutionResult> {
       return makeMockResolutionResult(did, this.didKeys.get(did)!);
     }
+  }
+
+  export type DataCID = { cid: CID, data: Uint8Array }
+
+  export async function makeDataCID(json_data: string): Promise<DataCID> {
+    const dataBytes = new TextEncoder().encode(json_data);
+    const cid = await generateCid(json_data);
+    return { cid: cid, data: dataBytes };
+  }
+
+  export function makeTestVerifiableCredential() {
+    return {
+        "@context": [
+          "https://www.w3.org/2018/credentials/v1",
+          "https://www.w3.org/2018/credentials/examples/v1"
+        ],
+        "id": "http://example.gov/credentials/3732",
+        "type": [
+          "VerifiableCredential",
+          "UniversityDegreeCredential"
+        ],
+        "issuer": "did:example:123",
+        "issuanceDate": "2020-03-10T04:24:12.164Z",
+        "credentialSubject": {
+          "id": "did:example:456",
+          "degree": {
+            "type": "BachelorDegree",
+            "name": "Bachelor of Science and Arts"
+          }
+        },
+        "proof": {
+          "type": "EcdsaSecp256k1Signature2019",
+          "created": "2020-03-10T04:24:12.164Z",
+          "proofPurpose": "assertionMethod",
+          "verificationMethod": "did:example:123#key1",
+          "jws": "eyJhbGciOiJFUzI1NksifQ.."
+        }
+      }
   }
--- tests/message.test.ts
+++ tests/message.test.ts
@@ -1,7 +1,7 @@
 import { DWN } from "../source/dwn-sdk";
 import createDWN from "../source/dwn-sdk-wrapper";
 import { MessageStoreMem } from "../source/dwn-sdk/store/message-store-mem";
-import { KeyPair, makeKeyPair, makeTestJWS, TestMethodResolver } from "./helpers";
+import { KeyPair, makeDataCID, makeKeyPair, makeTestJWS, makeTestVerifiableCredential, TestMethodResolver } from "./helpers";
 
 describe("test message handling", () => {
 
@@ -57,6 +57,39 @@ describe("test message handling", () => {
         }
       ]
     }
+    const res = await dwn.processRequest(messageBody);
+    await expect(res.replies).toHaveLength(1);
+    await expect(res.replies![0].status.code).toBe(200);
+  })
+
+  test("object storage", async () => {
+
+    const data = makeTestVerifiableCredential();
+    const dataCid = await makeDataCID(JSON.stringify(data));
+
+    const descriptor = {
+      "nonce": "9b9c7f1fcabfc471ee2682890b58a427ba2c8db59ddf3c2d5ad16ccc84bb3106",
+      "method": "CollectionsWrite",
+      "schema": "https://schema.org/SocialMediaPosting",
+      "recordId": "b6464162-84af-4aab-aff5-f1f8438dfc1e",
+      "dataCid": Buffer.from(dataCid.cid.bytes).toString('base64'),
+      "dateCreated": 123456789,
+      "dataFormat": "application/json"
+    };
+
+    const jws = await makeTestJWS(descriptor, keyPair, testDid);
+    
+    const messageBody  = {
+      "target": "did:example:123",
+      "messages": [
+        {
+          "data": Buffer.from(dataCid.data).toString('base64'),
+          "descriptor": descriptor,
+          "authorization": jws
+        }
+      ]
+    }
+    console.log(JSON.stringify(messageBody))
     const res = await dwn.processRequest(messageBody);
     await expect(res.replies).toHaveLength(1);
     await expect(res.replies![0].status.code).toBe(200);
