026bfd5dfa3c7c0abfc8ff6eb2c62a1be46373ea
Peter O'Leary <pete@mudflapinc.com>
2022-09-18 08:20:14 -0700

    Fixed a few problems with SimpleIndex it is working now

--- source/dwn-sdk/store/simple-index.ts
+++ source/dwn-sdk/store/simple-index.ts
@@ -14,11 +14,11 @@ export default class SimpleIndex {
 
         // schemas MUST contain descriptor.method OR data and descriptor.schema
 
-        this.keys = []
+        this.keys = new Array()
         partial_messages.forEach(partial_message => {
             const flattened = this.flatten(partial_message);
-            const keys = Object.keys(flattened);
-            this.keys = this.keys.concat(keys);
+            const keys = flattened.keys();
+            this.keys = this.keys.concat([...keys]);
         })
 
         this.documents = new Map();
@@ -43,7 +43,7 @@ export default class SimpleIndex {
                 ret = new Map([...ret, ...this.flatten(obj[attr], key)]);
             }
             else{
-                ret[key] = obj[attr];
+                ret.set(key, obj[attr]);
             }
         }
         return ret;
@@ -52,10 +52,10 @@ export default class SimpleIndex {
     put(id: string, message: any): void {
         const flattened = this.flatten(message);
         for (const key of this.keys) {
-            if (flattened[key]) {
+            if (flattened.get(key)) {
                 const existingIds = this.documents.get(key) || new Map();
                 const existingValues = existingIds.get(id) || [];
-                existingIds.set(id, existingValues.concat(flattened[key]));
+                existingIds.set(id, existingValues.concat(flattened.get(key)));
                 this.documents.set(key, existingIds);
             }
         }
@@ -65,7 +65,7 @@ export default class SimpleIndex {
         const flattened = this.flatten(query);
         const ret: string[] = []
         for (const [query_key, query_value] of flattened) {
-            if (this.documents[query_key]) {
+            if (this.documents.get(query_key)) {
                 const existingIds = this.documents.get(query_key);
                 if (existingIds) {
                     for (const [id, values] of existingIds) {
