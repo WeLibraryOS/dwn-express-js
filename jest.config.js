a560c25c642d3778b25896535ce45d7204004817
Peter O'Leary <pete@mudflapinc.com>
2022-12-26 10:37:46 -0800

    Tests are running again under Typescript but they are failing

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+var esmModules = ['key-id-resolver'];
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.[jt]sx?$': ['esbuild-jest', { sourcemap: true, target: 'es2020' }],
+},
+  transformIgnorePatterns: [
+  ],
 };
